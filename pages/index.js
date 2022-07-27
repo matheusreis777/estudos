4afcdc7d4e25d480117f6db4a43aabb147907bcb
matheusreis777 <matheuscronapp@gmail.com>
2022-07-27 14:34:26 -0400

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home () {
-    return <div><h1>Home</h1></div>
+    return <div><center><h1>TE AMO MINHA BB</h1>
+    <img src="https://static-wp-tor15-prd.torcedores.com/wp-content/uploads/2016/04/12191914_996696590369533_1764170945002133538_n-590x259.jpg" alt="que coisa em"></img>
+
+    <h2>HOJE SEU TIME VAI GANHAR E VAI DAR TUDO CERTO!!!</h2>
+    </center>
+    </div>
 }
 
-export default Home
+export default Home 
